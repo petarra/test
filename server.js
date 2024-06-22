232bf5e30856867869bf17fa97682f132d9f0390
Petani Kode <contoh@petanikode.com>
2024-06-23 02:55:50 +0700

    done?! 7

--- server.js
+++ server.js
@@ -12,17 +12,6 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(session({
-  genid: req => uuidv4(), // Use UUIDs for session IDs
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-      secure: false, // Set to true if using HTTPS
-      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days in milliseconds
-  }
-}));
-
 // load static assets
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
