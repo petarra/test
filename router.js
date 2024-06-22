9348f26f34870ae29123d57066b4b5c30df708b6
Petani Kode <contoh@petanikode.com>
2024-06-23 02:04:34 +0700

    done?!

--- router.js
+++ router.js
@@ -22,23 +22,27 @@ const dbConfig = {
     queueLimit: 0
 };
 
+const connection = mysql.createConnection(dbConfig);
+    connection.connect((err) => {
+        if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+        }
+        console.log('Connected to the MySQL database.');
+});
+
 // Create a connection pool
 const pool = mysql.createPool(dbConfig);
 
-// Promisify query method to use async/await
 const query = promisify(pool.query).bind(pool);
 
 // multer for local storage
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, 'public', 'assets', 'product'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // Max file size is 5MB
+  },
 });
-
-const upload = multer({ storage: storage });
 
 // Middleware to check if user is authenticated
 const authenticateUser = (req, res, next) => {
@@ -112,7 +116,7 @@ router.get('/image/:imageName', (req, res) => {
   });
 
 // Add data to table
-router.post('/add', upload.single('fileimage'), async (req, res) => {
+router.post('/add', upload.single('fileimage'), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
@@ -122,68 +126,138 @@ router.post('/add', upload.single('fileimage'), async (req, res) => {
     }
 
     const { table, name } = req.body;
-    const file = bucket.file(`assets/product/${req.file.originalname}`);
+    const img = req.file.originalname;
+    // Check if img already exists in the table
+    connection.query(`SELECT * FROM ${table} WHERE img = ?`, [img], (err, result) => {
+        if (err) {
+            console.error('Error querying the database:', err);
+            return res.status(500).send('Database query failed');
+        }
 
-    try {
-        await file.save(req.file.buffer, {
-            metadata: { contentType: req.file.mimetype }
-        });
-
-        const img = `https://firebasestorage.googleapis.com/v0/b/${process.env.FIREBASE_STORAGE_BUCKET}/o/assets%2Fproduct%2F${encodeURIComponent(req.file.originalname)}?alt=media`;
-
-        const [rows] = await pool.query(`SELECT * FROM ${table} WHERE img = ?`, [img]);
-
-        if (rows.length > 0) {
+        if (result.length > 0) {
             return res.status(400).json({ message: 'Image already being used' });
         }
 
-        await pool.query(`INSERT INTO ${table} (name, img) VALUES (?, ?)`, [name, img]);
-        res.redirect(`/admin?table=${table}`);
-    } catch (err) {
-        console.error('Error uploading to Firebase:', err);
-        res.status(500).send('Firebase upload failed');
-    }
+        const imageName = req.file.originalname;
+        const file = bucket.file(imageName);
+        const stream = file.createWriteStream({
+            metadata: {
+              contentType: req.file.mimetype,
+            },
+          });
+      
+          stream.on('error', (err) => {
+            console.error('Error uploading file:', err);
+            res.status(500).send('Error uploading file.');
+          });
+      
+          stream.end(req.file.buffer);
+        // Proceed with insertion if img is unique
+        connection.query(`INSERT INTO ${table} (name, img) VALUES (?, ?)`, [name, img], (err, result) => {
+            if (err) {
+                console.error('Error inserting into the database:', err);
+                return res.status(500).send('Database insert failed');
+            }
+            res.redirect(`/admin?table=${table}`);
+        });
+    });
 });
 
 // Update data in table
-router.post('/update', upload.single('newimage'), async (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded' });
-    }
-
-    const { table, id, name } = req.body;
-    const file = bucket.file(`assets/product/${req.file.originalname}`);
-
+router.post('/update/:imageName', upload.single('newimage'), (req, res) => {
     try {
-        await file.save(req.file.buffer, {
-            metadata: { contentType: req.file.mimetype }
-        });
+        const { table, id, name } = req.body;
 
-        const img = `https://firebasestorage.googleapis.com/v0/b/${process.env.FIREBASE_STORAGE_BUCKET}/o/assets%2Fproduct%2F${encodeURIComponent(req.file.originalname)}?alt=media`;
+        // If no new image uploaded, just update the name
+        if (!req.file) {
+            pool.query(`UPDATE ${table} SET name = ? WHERE id = ?`, [name, id], (err, result) => {
+                if (err) {
+                    console.error('Error updating MySQL database:', err);
+                    res.status(500).send('MySQL update failed');
+                } else {
+                    res.redirect(`/admin?table=${table}`);
+                }
+            });
+        } else {
+            const delImage = req.params.imageName;
+            const del = bucket.file(delImage);
 
-        await pool.query(`UPDATE ${table} SET name = ?, img = ? WHERE id = ?`, [name, img, id]);
-        res.redirect(`/admin?table=${table}`);
+            // Delete old image from Firebase Storage
+            del.delete((err, apiResponse) => {
+                if (err) {
+                    console.error('Error deleting old image from Firebase:', err);
+                    return res.status(500).send('Error deleting old image from Firebase.');
+                }
+
+                const imageName = req.file.originalname;
+                // Upload new image to Firebase Storage
+                const file = bucket.file(imageName);
+                const stream = file.createWriteStream({
+                    metadata: {
+                        contentType: req.file.mimetype,
+                    },
+                });
+
+                stream.on('error', (err) => {
+                    console.error('Error uploading file:', err);
+                    res.status(500).send('Error uploading file.');
+                });
+
+                stream.on('finish', () => {
+                    // Update record in MySQL database
+                    pool.query(`UPDATE ${table} SET name = ?, img = ? WHERE id = ?`, [name, imageName, id], (err, result) => {
+                        if (err) {
+                            console.error('Error updating MySQL database:', err);
+                            res.status(500).send('MySQL update failed');
+                        } else {
+                            res.redirect(`/admin?table=${table}`);
+                        }
+                    });
+                });
+
+                stream.end(req.file.buffer);
+            });
+        }
     } catch (err) {
-        console.error('Error uploading to Firebase:', err);
-        res.status(500).send('Firebase upload failed');
+        console.error('Error updating:', err);
+        res.status(500).send('Update operation failed.');
     }
 });
 
-// Delete data from table
-router.post('/delete', async (req, res) => {
-    const { table, id, img } = req.body;
-    const fileName = decodeURIComponent(path.basename(img));
 
-    const file = bucket.file(`assets/product/${fileName}`);
+
+// Delete data from table
+router.post('/delete/:imageName', async (req, res) => {
+    const { table, id } = req.body;
+    const fileName = req.params.imageName;
+
+    const file = bucket.file(fileName);
 
     try {
+        // Check if the file exists in Firebase Storage
+        const [exists] = await file.exists();
+        if (!exists) {
+            return res.status(404).send('File not found.');
+        }
+
+        // Delete the file from Firebase Storage
         await file.delete();
 
-        await pool.query(`DELETE FROM ${table} WHERE id = ?`, [id]);
+        // Delete the corresponding record from the MySQL database table
+        await new Promise((resolve, reject) => {
+            pool.query(`DELETE FROM ${table} WHERE id = ?`, [id], (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+
         res.redirect(`/admin?table=${table}`);
     } catch (err) {
-        console.error('Error deleting file from Firebase:', err);
-        res.status(500).send('Firebase delete failed');
+        console.error('Error deleting file or record:', err);
+        res.status(500).send('Delete operation failed.');
     }
 });
 
@@ -245,17 +319,26 @@ router.get('/dashboardSearch', authenticateUser, async (req, res) => {
 });
 
 // Search across all tables
-router.get('/search', authenticateUser, async (req, res) => {
+router.get('/search', async (req, res) => {
     const searchTerm = req.query.term;
-    const query = 'SELECT * FROM tangki WHERE name LIKE ? UNION SELECT * FROM pipa WHERE name LIKE ? UNION SELECT * FROM bahan_saringan WHERE name LIKE ? UNION SELECT * FROM sambungan WHERE name LIKE ?';
+    const sql = `
+        SELECT * FROM tangki WHERE name LIKE ? 
+        UNION 
+        SELECT * FROM pipa WHERE name LIKE ? 
+        UNION 
+        SELECT * FROM bahan_saringan WHERE name LIKE ? 
+        UNION 
+        SELECT * FROM sambungan WHERE name LIKE ?`;
+
     try {
-        const rows = await query(query, [`%${searchTerm}%`, `%${searchTerm}%`, `%${searchTerm}%`, `%${searchTerm}%`]);
+        const rows = await query(sql, [`%${searchTerm}%`, `%${searchTerm}%`, `%${searchTerm}%`, `%${searchTerm}%`]);
         res.render('search', { user: req.session.user, data: rows });
     } catch (err) {
         console.error('Error querying the database:', err);
         res.status(500).send('Database query failed');
     }
 });
+
 
 
 // Logout route
