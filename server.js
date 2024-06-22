6e94316e5b0b0ab3454287ddeef6dc58f3b004cb
Petani Kode <contoh@petanikode.com>
2024-06-23 02:24:42 +0700

    done?! 3

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyparser = require("body-parser");
 const session = require("express-session");
 const { v4: uuidv4 } = require("uuid");
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +13,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(session({
-  secret: uuidv4(), //  '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+  secret: process.env.SECRET, 
   resave: false,
   saveUninitialized: true
 }));
